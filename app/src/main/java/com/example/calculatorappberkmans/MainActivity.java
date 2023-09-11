8854ee1eef71352ea13bb36cf81e377e336bb5f1
AdrianBerkmans <Adrian.berkmans@gmail.com>
2023-09-10 21:53:18 -0500

    Continuing Work On Functionality

--- app/src/main/java/com/example/calculatorappberkmans/MainActivity.java
+++ app/src/main/java/com/example/calculatorappberkmans/MainActivity.java
@@ -17,40 +17,44 @@ public class MainActivity extends AppCompatActivity {
     public void onClick(View v){
         TextView Output = findViewById(R.id.textViewOutput);
         TextView Previous = findViewById(R.id.textViewPrevious);
+        Previous.setText(Output.getText());
         String Equation = "";
         int CurId = v.getId();
         if(CurId == R.id.button1){
-            Equation += "1";
+            Equation = "1";
         }
         else if(CurId == R.id.button2){
-            Equation += "2";
+            Equation = "2";
         }
         else if(CurId == R.id.button3){
-            Equation += "3";
+            Equation = "3";
         }
         else if(CurId == R.id.button4){
-            Equation += "4";
+            Equation = "4";
         }
         else if(CurId == R.id.button5){
-            Equation += "5";
+            Equation = "5";
         }
         else if(CurId == R.id.button6){
-            Equation += "6";
+            Equation = "6";
         }
         else if(CurId == R.id.button7){
-            Equation += "7";
+            Equation = "7";
         }
         else if(CurId == R.id.button8){
-            Equation += "8";
+            Equation = "8";
         }
         else if(CurId == R.id.button9){
-            Equation += "9";
+            Equation = "9";
         }
         else if(CurId == R.id.buttonDecimal){
-            Equation += ".";
+            Equation = ".";
+        }
+        else if(CurId == R.id.buttonEqual){
+
         }
 
-        Output.setText(Equation);
+        Output.setText(Previous.getText()+Equation);
     }
 
 }
